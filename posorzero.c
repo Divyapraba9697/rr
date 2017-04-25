240fb84300cddfbf7944c7b344d7987a3c535af3
Divyapraba9697 <divyapraba9697@gmail.com>
2017-04-25 09:27:27 +0530

    Create posorzero.c

---
+++ posorzero.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+{
+printf("a is postive");
+}
+ifelse(a<0)
+{
+printf("a is negative");
+}
+else
+{
+printf("a is zero");
+}
+return 0;
+}
