bd03fe2d2838c5b7dee87e66b6106a3853dbb580
Divyapraba9697 <divyapraba9697@gmail.com>
2017-04-25 10:14:47 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+scanf("%s",&a);
+if((a=='a')||(a=='e')||(a=='i')||(a=='o')||(a=='u')||(a=='A')||(a=='E')||(a=='I')||(a=='O')||(a=='U'))
+{
+printf("a is vowel");
+}
+else
+{
+printf("a is constant");
+}
+return 0;
+}
